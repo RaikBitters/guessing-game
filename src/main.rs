7f9fffde15f0c1a939fe358c2290ee6360b5dd87
RaikBitters <raikbitters@MacBook-Air-RaikBitters.local>
2020-07-11 12:42:25 +0300

    Hide secret number printing

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,7 @@ fn main() {
     let secret_number = rand::thread_rng().gen_range(1,101);
 
     // For testing.
-    // TODO Delete in the end.
-    println!("The secret number is: {}", secret_number);
+    //println!("The secret number is: {}", secret_number);
     
     loop {
         println!("Please input your guess.");
